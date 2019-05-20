d1e84225a71a8942fc410cee86b6123fd9984223
Muhammad Yousuf <m.yousuf.go@gmail.com>
2019-05-20 23:15:50 +0500

    validations applied

--- app/src/main/java/com/example/saloonapp/Activities/SignupActivity.java
+++ app/src/main/java/com/example/saloonapp/Activities/SignupActivity.java
@@ -4,16 +4,20 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.saloonapp.Adapters.SignupViewPagerAdapter;
 import com.example.saloonapp.Fragments.ParlourSignupFragment;
 import com.example.saloonapp.Fragments.UserSignupFragment;
 import com.example.saloonapp.R;
 
-public class SignupActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private AppCompatImageView backIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +25,18 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         bindControls();
+        bindListeners();
         setUpTabs();
+    }
+
+    private void bindListeners() {
+        backIV.setOnClickListener(this);
     }
 
     private void bindControls() {
         tabLayout = findViewById(R.id.signup_TL);
         viewPager = findViewById(R.id.signup_VP);
+        backIV = findViewById(R.id.signup_backIV);
     }
 
     private void setUpTabs() {
@@ -35,5 +45,12 @@ public class SignupActivity extends AppCompatActivity {
         adapter.AddFragment(new ParlourSignupFragment(), "As Parlour");
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == backIV) {
+            onBackPressed();
+        }
     }
 }
